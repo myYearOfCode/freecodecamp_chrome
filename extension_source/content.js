79dcd4e91f781ff4a40aaa48c657899f16efdca7
myYearOfCode <40813606+myYearOfCode@users.noreply.github.com>
2018-11-08 15:05:13 -0500

    remove comments on failed features

--- extension_source/content.js
+++ extension_source/content.js
@@ -1,15 +1,3 @@
-// select all of the lessons
-// of the first section 
-// of the first open certification 
-// and convert it from a nodelist to an array
-// let itemsArray = [...document.getElementsByClassName("superblock open")[0].getElementsByTagName('ul')[0].getElementsByTagName('li')[0].getElementsByTagName('ul')[0].getElementsByTagName('li')]
-
-// // delete each lesson
-// itemsArray.map(item => {item.innerHTML=""})
-
-// // toggle the arrow on the open section
-// document.getElementsByClassName("block open")[0].className="block"
-
 // select all of the sections
 // of the first open certification 
 // and convert it from a nodelist to an array
@@ -20,3 +8,6 @@ lessonsArray.map(item => {item.innerHTML=""})
 
 // toggle the arrow on the open certificate
 document.getElementsByClassName("superblock open")[0].className="superblock"
+
+//this will require two clicks on the top arrow in order to reset it's state and then reveal it again. 
+//I don't think I can modify the react state from a chrome plugin, or else I will do that.
