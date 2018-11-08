d4c8cbb69cbb4553312318f71ce47572b68531ae
myYearOfCode <40813606+myYearOfCode@users.noreply.github.com>
2018-11-08 15:37:02 -0500

    cleaning up comments

--- extension_source/content.js
+++ extension_source/content.js
@@ -1,13 +1,13 @@
-// select all of the sections
-// of the first open certification 
+// select the first certification 
+// select all of the sub sections
 // and convert it from a nodelist to an array
 let lessonsArray = [...document.getElementsByClassName("superblock open")[0].getElementsByTagName('ul')]
 
-// delete each lesson
+// delete the inner html for each lesson so it is no longer visible
 lessonsArray.map(item => {item.innerHTML=""})
 
-// toggle the arrow on the open certificate
+// toggle the arrow on the open certificate to make it look like it is closed
 document.getElementsByClassName("superblock open")[0].className="superblock"
 
-//this will require two clicks on the top arrow in order to reset it's state and then reveal it again. 
-//I don't think I can modify the react state from a chrome plugin, or else I will do that.
+// this will require two clicks on the top arrow in order to reset it's state and then reveal it again. 
+// I don't think I can modify the react state from a chrome plugin, or else I will do that.
